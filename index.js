337e57c5ec47d1fe036c4878bb6bb3d43db8a657
Devin Pierce <devinmpierce@gmail.com>
2018-04-11 18:30:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return arr;
 }
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] += '!!!'
+    i++;
+  }
+}
